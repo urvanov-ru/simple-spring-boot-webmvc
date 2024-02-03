5446ca6a6e47336a75a568154f3fedbc331d5138
Fedor Urvanov <fedor@urvanov.ru>
2024-02-03 11:58:05 +0300

    Spring boot simple webmvc first commit

---
+++ src/main/java/ru/urvanov/springbook2024/simplespringbootwebmvc/SimpleSpringBootWebmvcApplication.java
@@ -0,0 +1,13 @@
+package ru.urvanov.springbook2024.simplespringbootwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootWebmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/urvanov/springbook2024/simplespringbootwebmvc/SimpleSpringBootWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urvanov.springbook2024.simplespringbootwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
